7cb01fe03afb3bd8457c9efc4b060b368014925e
Pristajchuk-Humain <104837383+Pristajchuk-Humain@users.noreply.github.com>
2022-07-21 13:40:27 +0200

    tetst

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,53 +1,27 @@
-import { Form, Formik, Field } from 'formik';
 
+  import React from 'react';
+  import { Formik, Form, Field, ErrorMessage } from 'formik';
+  
+  const Searchbar = ({onSubmit, isSubmiting}) => {
+    const handleSubmit = (values, actions) => {
+      onSubmit(values)
+      actions.resetForm();
 
- const Searchbar = () => (
-        <Formik
-        initialValues={{ search: '' }}
-        validate={values => {
-          const errors = {};
-          if (!values.search) {
-            errors.search = 'search';
-          } else if (
-            !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.search)
-          ) {
-            errors.search = 'This picture not found';
-          }
-          return errors;
-        }}
-        onSubmit={(values, { setSubmitting }) => {
-          setTimeout(() => {
-            alert(JSON.stringify(values, null, 2));
-            setSubmitting(false);
-          }, 400);
-        }}
-      >
-        {({
-          values,
-          errors,
-          touched,
-          handleChange,
-          handleBlur,
-          handleSubmit,
-          isSubmitting,
-          /* and other goodies */
-        }) => (
-          <Form onSubmit={handleSubmit}>
-            <input
-              type="search"
-              name="search"
-              onChange={handleChange}
-              onBlur={handleBlur}
-              value={values.search}
-            />
-            {errors.search && touched.search && errors.search}
-            <button type="submit" disabled={isSubmitting}>
+    }
+    return (
+      <Formik
+        initialValues={{ search: " " }}
+        onSubmit={handleSubmit}>
+          <Form>
+            <Field type="text" name="search" />
+            <button type="submit" desebled={isSubmiting}>
               Submit
             </button>
-      </Form>
-        )}
+          </Form>
       </Formik>
-    
- )
+    )
+  }
+  
+  export default Searchbar
 
-export default Searchbar
+
